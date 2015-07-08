9e3bc88069b5e86041ac952aca8aaa307680d4bb
Chris Ruppel <chris.ruppel@gmail.com>
2015-07-08 12:04:11 +0200

    :bulb: separate beacon and visualization

--- public/js/client.js
+++ public/js/client.js
@@ -4,19 +4,6 @@ var $ = function (selector) { return document.querySelector(selector); }
 var socket = io();
 
 /**
- * Add a new screen on all clients. It doesn't immediately add a shape to your
- * DOM, the 'add' listener below handles that part.
- */
-window.onload = function() {
-  socket.emit('add', {
-    id: 'screen-' + Math.floor(Math.random() * 1000000000),
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')',
-  });
-};
-
-/**
  * Listen for new screens and add them to DOM.
  */
 socket.on('add', function(props) {
@@ -24,8 +11,8 @@ socket.on('add', function(props) {
   var el = document.createElement('div');
   el.id = props.id;
   el.classList.add('screen');
-  el.style.width = props.width + 'px';
-  el.style.height = props.height + 'px';
+  el.style.width = props.w + 'px';
+  el.style.height = props.h + 'px';
   el.style.backgroundColor = props.backgroundColor;
 
   // Add the new element
