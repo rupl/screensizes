edb104d62b11df5b145a4f8f3d76ed030f84367b
Chris Ruppel <chris.ruppel@gmail.com>
2022-09-28 16:55:22 +0200

    :arrow_up: node.js 18 plus address breaking changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,22 @@ var c = u.colors;
 
 // Project deps
 var nodemon = require('gulp-nodemon');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var prefix = require('gulp-autoprefixer');
 
 // Log some debug info
 log(c.yellow('Environment:', (process.env.NODE_ENV || 'local')));
 log(c.yellow('Visualize Self:', (process.env.VISUALIZE_SELF || 'false')));
 
-// -----------------------------------------------------------------------------
+
+//——————————————————————————————————————————————————————————————————————————————
 // Sass Task
 //
 // Compiles Sass and runs the CSS through autoprefixer.
-// -----------------------------------------------------------------------------
-gulp.task('sass', function() {
+//——————————————————————————————————————————————————————————————————————————————
+function sassTask() {
   return gulp.src('sass/**/*')
-    .pipe(sass({
-        outputStyle: 'nested',
-      })
+    .pipe(sass()
       .on('error', function(err, res) {
         log(c.red('sass'), 'failed to compile');
         log(c.red('> ') + err.message);
@@ -30,19 +29,25 @@ gulp.task('sass', function() {
     )
     .pipe(prefix('last 2 versions', '> 1%'))
     .pipe(gulp.dest('public/css'));
-});
+};
+sassTask.description = 'Compiles Sass';
+module.exports.sass = sassTask;
 
-// -----------------------------------------------------------------------------
+
+//——————————————————————————————————————————————————————————————————————————————
 // Watch tasks
-// -----------------------------------------------------------------------------
-gulp.task('watch', function() {
-  gulp.watch('sass/**/*', ['sass']);
-});
+//——————————————————————————————————————————————————————————————————————————————
+function watchTask() {
+  gulp.watch('sass/**/*', sassTask);
+};
+watchTask.description = 'For local development';
+module.exports.watch = watchTask;
 
-// -----------------------------------------------------------------------------
+
+//——————————————————————————————————————————————————————————————————————————————
 // Run the dev server
-// -----------------------------------------------------------------------------
-gulp.task('start', ['sass', 'watch'], function () {
+//——————————————————————————————————————————————————————————————————————————————
+gulp.task('start', gulp.series(sassTask, 'watch'), () => {
   nodemon({
     script: 'index.js',
     ext: 'js html dust',
@@ -50,7 +55,8 @@ gulp.task('start', ['sass', 'watch'], function () {
   });
 });
 
-// -----------------------------------------------------------------------------
+
+//——————————————————————————————————————————————————————————————————————————————
 // Default should just start the server
-// -----------------------------------------------------------------------------
-gulp.task('default', ['start']);
+//——————————————————————————————————————————————————————————————————————————————
+gulp.task('default', gulp.parallel('start'));
